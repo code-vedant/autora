05c95e1fa05132ba9caeeacee4654e2e55c0d8d5
code-vedant <162135022+code-vedant@users.noreply.github.com>
2025-05-26 10:29:14 +0530

    changes

--- src/app/(auth)/layout.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from "react";
-
-const AuthLayout = ({ children }:  {children : ReactNode}) => {
-    return <div className="flex items-center justify-center h-screen">{children}</div>;
-  };
-  
-  export default AuthLayout;
--- src/app/(main)/layout.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const MainLayout = ({ children }: {children : React.ReactNode}) => {
-  return <div className="container mx-auto mt-8 md:mt-14">{children}</div>;
-};
-
-export default MainLayout;
